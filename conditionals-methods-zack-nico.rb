e0a202609773f4f09a0f94d6e8bc8c2e17edb723
ZackRohde <zdrohde@outlook.com>
2022-09-28 12:02:42 -0700

    RPS Started

--- conditionals-methods-zack-nico.rb
+++ conditionals-methods-zack-nico.rb
@@ -32,3 +32,25 @@ end
 p valid_palindrome('anna')
 
 
+puts 'first user, enter your name'
+name1 = gets.chomp
+puts "Hello #{name1}, are you ready to play?"
+
+puts 'second user, enter your name'
+name2 = gets.chomp
+puts "Hello #{name2}, are you ready to play?"
+
+puts 'first user pick rock, paper, or scissors'
+choice1 = gets.chomp
+
+puts 'second user pick rock, paper, or scissors'
+choice2 = gets.chomp
+
+def rps(name1, name2, choice1, choice2)
+if (choice1.downcase == 'rock' && choice2.downcase == 'scissors')
+        "#{name1} wins"
+    # elsif (second_user == 'rock' && first_user == 'scissors')
+    #     second_user wins
+    end
+end
+p rps(name1, name2, choice1, choice2)
