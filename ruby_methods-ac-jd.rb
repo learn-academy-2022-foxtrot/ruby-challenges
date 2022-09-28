ad1e1d9f2cf4fecd30946b7b33cd9caf2b6a116d
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-09-28 10:33:52 -0700

    first set of challenges done

---
+++ ruby_methods-ac-jd.rb
@@ -0,0 +1,59 @@
+# Challenges
+# Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
+# def sum_these_numbers(num1,num2)
+#    p num1 + num2
+# end
+
+# sum_these_numbers(1,2)
+
+
+
+# Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
+# def is_even(num)
+#     if num.even?
+#         true
+#     else
+#         false
+#     end
+# end 
+
+# p is_even(2)
+# p is_even(7)
+# Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+# def between_one_and_ten(num)
+#     if num >= 1 && num <= 10
+#         puts 'valid'
+#     else 
+#         puts 'invalid'
+#     end
+# end
+
+# between_one_and_ten(5)
+# between_one_and_ten(11)
+
+# Create a method that takes in a string and determines if the string is a palindrome.
+# def palindromeCheck(string)
+#     if string == string.reverse
+#         p "#{string} is a palindrome"
+#     else 
+#         p "#{string} is not a palindrome"
+#     end
+# end
+
+# palindromeCheck('leather')
+# palindromeCheck('racecar')
+
+
+
+
+# Challenge: Rock, Paper, Scissors
+# As the first user, I can see a prompt in the terminal to enter my name.
+# As the second user, I can see a prompt in the terminal to enter my name.
+# As the first user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+# As the second user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+# As a user, I can see a message in the terminal depicting which user won the round.
+# As a user, I can see a message in the terminal noting if there was a tie.
+
+
+
+
