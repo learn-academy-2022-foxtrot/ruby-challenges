c5319fb7ba93bf4c8a95d1008de3a4d3bfffded7
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-10-03 11:58:13 -0700

    first portion complete, color palette needs work

--- classes-christian-matthew.rb
+++ classes-christian-matthew.rb
@@ -3,18 +3,60 @@
 # For the following Task challenge use initialize, setter, and getter methods for your class.
 
 # As a developer, I can create a class called Task.
-class Task
+# class Task
 
-end
+# end
 # As a developer, I can create three instances (objects) of class Task. e.g laundry = Task.new
-p laundry = Task.new
-#<Task:0x00007fc30583d4f0>
+# p laundry = Task.new
+# #<Task:0x00007fc30583d4f0>
+# p dishes = Task.new
+# #<Task:0x00007fdb5e80cc08>
+# p vaccuum = Task.new
+# #<Task:0x00007f7f8682d108>
 
 # As a developer, I can initialize each instance of class Task with a title.
-
 # As a developer, I can see the title of each instance of class Task.
+class Task
+    attr_accessor :title
+    def initialize(title)
+        @title = title
+        @status = 'incomplete'
+    end
+    
+    def get_title
+        @title
+    end
+    
+    def get_status
+        @status
+    end
+
+    def chore_complete
+        @status = 'complete'
+    end
+end
+
+laundry = Task.new('laundry')
+laundry.chore_complete
+p laundry
+
+dishes = Task.new('dishes')
+dishes.chore_complete
+p dishes
+
+vaccuum = Task.new('vaccuum')
+vaccuum.chore_complete
+p vaccuum
+# "laundry"
+# "dishes"
+# "vaccuum"
 
 # As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
+
+#<Task:0x00007fc72e083638 @title="laundry", @status="incomplete">
+#<Task:0x00007fc72e083278 @title="dishes", @status="incomplete">
+#<Task:0x00007fc72e082bc0 @title="vaccuum", @status="incomplete">
+
 
 # As a developer, I can update the status of each instance of class Task when the task has been completed.
 
